b970310d14b180413137d74c6edd579edbb3d136
Matisse <dfink001@ucr.edu>
2013-04-01 23:59:38 -0700

    Added Constrain
    Constrain is added in this commit, it basically is used to constrain the
    data between two data points. This is potentially useful if the data
    falls out of range, this can correct that.

--- src/morphers/remap/Constrain.java
+++ src/morphers/remap/Constrain.java
@@ -1,5 +1,36 @@
 package morphers.remap;
 
-public class Constrain {
+import structure.DataNode;
+import structure.MathHelper;
+import abstracts.MorphRule;
+
+/**
+ * Constrains the data given to a range of min to max.
+ * 
+ * @Example initial: abcdefghi <br />
+ *          min = 'b'<br />
+ *          max = 'g'<br />
+ *          result: bbcdefggg
+ * @author Matisse
+ * 
+ */
+public class Constrain extends MorphRule {
+
+	int min, max;
+
+	public Constrain(DataNode ruleData, int min, int max) {
+		super(ruleData);
+		this.max = max;
+		this.min = min;
+	}
+
+	@Override
+	protected void noteMorph(DataNode input) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < data.length(); ++i) {
+			sb.append((char) MathHelper.constrain(data.get(i), min, max));
+		}
+		data.setNoteData(sb.toString());
+	}
 
 }
